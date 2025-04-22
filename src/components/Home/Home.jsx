90ef81633bb151c5c5a155b0738f285df4b85901
Eva <evarondamerlo2005@gmail.com>
2025-04-22 12:55:38 +0200

    pagina principal

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
         <p className="subtitle">
           <strong>
             ¡Bienvenido a Qualisophy!<br />
-            Formación práctica, consultoría personalizada y oportunidades reales de crecimiento. <br />
-            Te ayudamos a aprender, avanzar y conectar con el mundo profesional. <br />
+            Formación práctica, consultoría personalizada y oportunidades reales de crecimiento. 
+            Te ayudamos a aprender, avanzar y conectar con el mundo profesional. 
             Descubre nuestros cursos, servicios para empresas y opciones de colaboración.
           </strong>
         </p>
@@ -29,36 +29,36 @@ const Home = () => {
         <div className="home-sections">
           <div className="home-section">
             <img src="/public/formacionPrincipal.jpg" alt="Cursos" />
-            <h3>Cursos</h3><br />
-            <p>Formaciones prácticas y actualizadas.</p><br /><br />
+            <h3>Cursos</h3>
+            <p>Formaciones prácticas y actualizadas.</p>
             <a href="/cursos" className="button">Ver más</a>
           </div>
           <div className="home-section">
             <img src="/consultoriasPrincipal.jpg" alt="Consultoría" />
-            <h3>Consultoría</h3><br />
-            <p>Asesoría para tus proyectos.</p><br /><br /><br />
+            <h3>Consultoría</h3>
+            <p>Asesoría para tus proyectos.</p>
             <a href="/about" className="button">Descubre cómo</a>
           </div>
           <div className="home-section">
             <img src="/formacionempresPrincipal.jpg" alt="Formación Empresas" />
-            <h3>Formación Empresas</h3><br />
-            <p>Capacitación adaptada a tu equipo.</p><br /><br />
+            <h3>Formación Empresas</h3>
+            <p>Capacitación adaptada a tu equipo.</p>
             <a href="/formacion-empresas" className="button">Solicita info</a>
           </div>
           <div className="home-section">
             <img src="/Partnership.jpg" alt="Partnership" />
-            <h3>Partnership</h3><br />
-            <p>Conecta con nuestro talento.</p><br /><br /><br />
+            <h3>Partnership</h3>
+            <p>Conecta con nuestro talento.</p>
             <a href="/partnership" className="button">Colabora</a>
           </div>
         </div>
-
       </div>
     </div>
   );
 };
 
 export default Home;
+
 
 
 
