d5cc8225c5a0be41724a73446e92de520c59e38c
Eva <evarondamerlo2005@gmail.com>
2025-04-23 14:24:10 +0200

    cambio consultoria

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,8 +11,13 @@ const About = () => {
             <h1 className="about-title">Consultoría</h1>
             <p className="about-text">
               En Qualisophy, ofrecemos servicios de consultoría especializados para pequeñas empresas que buscan orientación técnica en sus proyectos, ya sean nuevos o existentes. Aportamos nuestra experiencia en calidad de software y desarrollo para ayudarte a tomar decisiones informadas, ahorrar costes y lograr resultados sostenibles.
-            </p> <br /><br />
+            </p> 
+            <br /><br />
+
+            {/* Imagen debajo del texto */}
+           
           </div>
+
           <div className="about-image">
             <a href="/contact" className="button">
               ¡Hablemos de tu proyecto!
@@ -20,6 +25,7 @@ const About = () => {
           </div>
         </div>
       </div>
+
       <section className="why-quark-techie">
         <div className="container">
           <h2>¿Por qué confiar en <font color="#567b7">Qualisophy</font>?</h2>
@@ -31,9 +37,15 @@ const About = () => {
             </p>
           </div>
         </div>
+        <img 
+              src="/public/logoqa.png" 
+              alt="Consultoría en acción" 
+              className="about-below-image" 
+            />
       </section>
     </div>
   );
 };
 
 export default About;
+
