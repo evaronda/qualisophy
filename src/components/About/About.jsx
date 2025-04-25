565ab08af1d932cb328533b1569f8efd561c9c41
mikzb <mikolajzabski@gmail.com>
2025-04-25 11:24:29 +0200

    añadido animaciones a about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import './About.css';
+import useAnimation from "../hooks/useAnimation";
 
 const About = () => {
+  useAnimation(".animated"); // Apply animations to elements with the "animated" class
+
   return (
     <div>
-      <div className="about-container">
-        <div className="about-content">
-          <div className="consultoria">
-            <h1 className="about-title">Consultoría</h1>
-            <p className="about-text">
+      <div className="about-container animated">
+        <div className="about-content animated">
+          <div className="consultoria animated">
+            <h1 className="about-title animated">Consultoría</h1>
+            <p className="about-text animated">
               En Qualisophy, ofrecemos servicios de consultoría especializados para pequeñas empresas que buscan orientación técnica en sus proyectos, ya sean nuevos o existentes. Aportamos nuestra experiencia en calidad de software y desarrollo para ayudarte a tomar decisiones informadas, ahorrar costes y lograr resultados sostenibles.
             </p>
           </div>
 
-          <div className="about-image">
-            <a href="/contact" className="button">
+          <div className="about-image animated">
+            <a href="/contact" className="button animated">
               ¡Hablemos de tu proyecto!
             </a>
           </div>
         </div>
       </div>
 
-      <section className="why-quark-techie">
-        <div className="container">
-          <h2>¿Por qué confiar en <font color="#567b7">Qualisophy</font>?</h2>
-          <div className="why-quark-techie-img-p">
-            <p className="why-quark-techie-p">
+      <section className="why-quark-techie animated">
+        <div className="container animated">
+          <h2 className="animated">¿Por qué confiar en <font color="#567b7">Qualisophy</font>?</h2>
+          <div className="why-quark-techie-img-p animated">
+            <p className="why-quark-techie-p animated">
               <b>
                 Si necesitas apoyo experto para lanzar, evaluar o mejorar tu producto digital, estás en el lugar correcto. Nuestro equipo ofrece soluciones personalizadas, eficaces y adaptadas al entorno tecnológico y necesidades de tu empresa.
               </b>
@@ -34,7 +37,7 @@ const About = () => {
           <img 
             src="/public/logoqa.png" 
             alt="Consultoría en acción" 
-            className="about-below-image" 
+            className="about-below-image animated" 
           />
         </div>
       </section>
