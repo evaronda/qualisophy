58eb5165f98f41232f3d8445ce66a70437b0e42b
mikzb <mikolajzabski@gmail.com>
2025-04-25 11:22:53 +0200

    arreglado estructura de codigo de About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './About.css';
 
 const About = () => {
-
   return (
     <div>
       <div className="about-container">
@@ -11,11 +10,7 @@ const About = () => {
             <h1 className="about-title">Consultoría</h1>
             <p className="about-text">
               En Qualisophy, ofrecemos servicios de consultoría especializados para pequeñas empresas que buscan orientación técnica en sus proyectos, ya sean nuevos o existentes. Aportamos nuestra experiencia en calidad de software y desarrollo para ayudarte a tomar decisiones informadas, ahorrar costes y lograr resultados sostenibles.
-            </p> 
-            <br /><br />
-
-            {/* Imagen debajo del texto */}
-           
+            </p>
           </div>
 
           <div className="about-image">
@@ -29,8 +24,8 @@ const About = () => {
       <section className="why-quark-techie">
         <div className="container">
           <h2>¿Por qué confiar en <font color="#567b7">Qualisophy</font>?</h2>
-          <div className='why-quark-techie-img-p'>
-            <p className='why-quark-techie-p'>
+          <div className="why-quark-techie-img-p">
+            <p className="why-quark-techie-p">
               <b>
                 Si necesitas apoyo experto para lanzar, evaluar o mejorar tu producto digital, estás en el lugar correcto. Nuestro equipo ofrece soluciones personalizadas, eficaces y adaptadas al entorno tecnológico y necesidades de tu empresa.
               </b>
