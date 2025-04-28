c4d4bdbab27a1c256d6e985b0906168b68f73fb4
Eva <evarondamerlo2005@gmail.com>
2025-04-28 12:30:02 +0200

    cambios·

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/qualisophy/', // Cambia la base a "/qualisophy/" para el deploy su GitHub Pages
+  plugins: [react()] 
 })
